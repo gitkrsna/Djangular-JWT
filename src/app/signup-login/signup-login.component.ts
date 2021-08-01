aee01fe81384b81dc013dcd3615695525b4442d4
Krishna <krishna@outplayhq.com>
2021-08-01 14:04:41 +0530

    week password logic added

--- src/app/signup-login/signup-login.component.ts
+++ src/app/signup-login/signup-login.component.ts
@@ -12,6 +12,8 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class SignupLoginComponent implements OnInit {
   public user: any;
+  weakPasswordMessage = ''
+  passwordStrength = 0
   email = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
 
@@ -25,6 +27,12 @@ export class SignupLoginComponent implements OnInit {
   }
 
   login() {
+    if (this.email.hasError('email') || this.email.hasError('required')) {
+      return
+    }  
+    if (this.passwordStrength < 40) {
+      return;
+    } 
     this.userService.login({
       username: this.user.username,
       password: this.user.password,
@@ -33,10 +41,60 @@ export class SignupLoginComponent implements OnInit {
     this.user.password = '';
   }
 
+getPasswordErrorMessage() {
+  if(this.user.password.length < 6) {
+    this.weakPasswordMessage = "Password should has atleast 6 charecters"
+  } else  if(this.user.password.length > 12) {
+    this.weakPasswordMessage = "Password can't have more than 12 charecters"
+  }
+  this.passwordStrength = this.checkStrength(this.user.password);
+  if (this.passwordStrength < 40) {
+    this.weakPasswordMessage = "Week Password";
+  } else {
+    this.weakPasswordMessage = '';
+  }
+}
+
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  checkStrength(p) {
+    // 1
+    let force = 0;
+  
+    // 2
+    const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
+    const lowerLetters = /[a-z]+/.test(p);
+    const upperLetters = /[A-Z]+/.test(p);
+    const numbers = /[0-9]+/.test(p);
+    const symbols = regex.test(p);
+  
+    // 3
+    const flags = [lowerLetters, upperLetters, numbers, symbols];
+  
+    // 4
+    let passedMatches = 0;
+    for (const flag of flags) {
+      passedMatches += flag === true ? 1 : 0;
+    }
+  
+    // 5
+    force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
+    force += passedMatches * 10;
+  
+    // 6
+    force = (p.length <= 6) ? Math.min(force, 10) : force;
+  
+    // 7
+    force = (passedMatches === 1) ? Math.min(force, 10) : force;
+    force = (passedMatches === 2) ? Math.min(force, 20) : force;
+    force = (passedMatches === 3) ? Math.min(force, 30) : force;
+    force = (passedMatches === 4) ? Math.min(force, 40) : force;
+  
+    return force;
   }
 }
