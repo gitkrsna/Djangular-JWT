d3b3020b625334a8356cadb71539a865c510a416
Krishna <krishna@outplayhq.com>
2021-08-01 13:32:04 +0530

    login-component modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,70 @@
 import { NgModule } from '@angular/core';
+
+// browser module
 import { BrowserModule } from '@angular/platform-browser';
+
+// browseranimation module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// http module
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+
+// form module
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// routing module
 import { AppRoutingModule } from './app-routing.module';
+
+// root component
 import { AppComponent } from './app.component';
+
+// material modules
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {A11yModule} from '@angular/cdk/a11y';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import {OverlayModule} from '@angular/cdk/overlay';
+
+// components 
 import { BlogPostService } from './services/blog_post.service';
 import { UserService } from './services/user.service';
 import { TokenInterceptor } from './services/http-interceptor.service';
@@ -11,10 +72,6 @@ import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
 import { SignupLoginComponent } from './signup-login/signup-login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,11 +84,57 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonToggleModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule,
+    A11yModule,
+    ClipboardModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    OverlayModule,
+    PortalModule,
+    ScrollingModule,
   ],
   
   providers: [BlogPostService, UserService,
--- src/app/signup-login/signup-login.component.ts
+++ src/app/signup-login/signup-login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { BlogPostService } from '../services/blog_post.service';
 import { UserService } from '../services/user.service';
 import { throwError } from 'rxjs'; // Angular 6/RxJS 6
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-login',
@@ -10,33 +11,13 @@ import { throwError } from 'rxjs'; // Angular 6/RxJS 6
   styleUrls: ['./signup-login.component.scss'],
 })
 export class SignupLoginComponent implements OnInit {
-  /**
-   * An object representing the user for the login form
-   */
   public user: any;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
 
-  /**
-   * An array of all the BlogPost objects from the API
-   */
-  public posts;
+  constructor(private userService: UserService) {}
 
-  /**
-   * An object representing the data in the "add" form
-   */
-  public new_post: any;
-
-  constructor(
-    private blogPostService: BlogPostService,
-    private router: Router,
-    private userService: UserService
-  ) {}
-
-  usertoken: any;
   ngOnInit() {
-    this.getPosts();
-    this.new_post = {
-      body: '',
-    };
     this.user = {
       username: '',
       password: '',
@@ -48,44 +29,14 @@ export class SignupLoginComponent implements OnInit {
       username: this.user.username,
       password: this.user.password,
     });
+    this.user.username = '';
+    this.user.password = '';
   }
 
-  refreshToken() {
-    this.userService.refreshToken();
-  }
-
-  logout() {
-    this.userService.logout();
-  }
-
-  getPosts() {
-    this.blogPostService.list().subscribe(
-      // the first argument is a function which runs on success
-      (data) => {
-        this.posts = data;
-        // convert the dates to a nice format
-        for (let post of this.posts.results) {
-          post.date = new Date(post.date);
-        }
-      },
-      // the second argument is a function which runs on error
-      (err) => console.error(err),
-      // the third argument is a function which runs on completion
-      () => console.log('done loading posts')
-    );
-  }
-
-  createPost() {
-    this.blogPostService.create(this.new_post, this.user.token).subscribe(
-      (data) => {
-        // refresh the list
-        this.getPosts();
-        return true;
-      },
-      (error) => {
-        console.error('Error saving!');
-        return throwError(error);
-      }
-    );
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 }
